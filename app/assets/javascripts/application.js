adf2267ad89c043702dcbc31b54e090712b8f39d
Nathnael Getahun <contact.nathnael@gmail.com>
2020-11-30 16:07:29 +0300

    Cleaned up scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,8 @@
-//= require example.js
+//= require FontAwesome.js
+//= require Chart.min.js
+//= require highmaps.js
+//= require data.js
+//= require exporting.js
+//= require drilldown.js
+//= require offline-exporting.js
+//= require Ethiopia-Regions.js
